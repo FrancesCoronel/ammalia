82f62fe9c971d209987428f11603d98619de9f89
stephzeid <stephaniezeidan4@gmail.com>
2016-02-13 17:15:42 -0800

    python setup and api work

---
+++ getImageURL.py
@@ -0,0 +1,10 @@
+import dropbox
+
+
+
+#Connect to dropbox 
+#List files from the folder
+#For loop to create URL's for all photos found in folder with +'&raw=1'
+#return a list of URLS and front end would get that list and render it in html
+#add them in a list (like array) outside of the for loop i return that list
+#
