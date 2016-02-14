c8a770731bd2300a4461911cd43c5833ec6dd79a
stephzeid <stephaniezeidan4@gmail.com>
2016-02-14 03:48:10 -0800

    a bunch of mentor stuff

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+app.debug = True
 dbx = dropbox.Dropbox('CzDOpqjLiK0AAAAAAAAEC4g3rnqNqhPkGHUqGwUrzopn6kIDf0UHJzC1vUdsu2T2')
-	dbx.users_get_current_account()
+dbx.users_get_current_account()
 
 @app.route('/')
 def home():
+	
+	return render_template('index.html')
+
+@app.route('/images')
+def images():
 	
 	#List files from the folder
 	entries = dbx.files_list_folder(path='/ammalia').entries
@@ -20,8 +25,9 @@ def home():
 		link.url += "&raw=1"
 		dropbox_images.append(link)
 	
+	#return dropbox_images
 	return render_template('images.html', images=dropbox_images)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
 
--- app/assets/js/min/parseDoctor-min.js
+++
@@ -1,1 +0,0 @@
-console.log("doctorinfo: "+JSON.stringify(doctorInfo));var doctor="Michael Scott Anderson";for(doctor in doctorInfo)console.log("doctor: "+JSON.stringify(doctorInfo.doctor));
