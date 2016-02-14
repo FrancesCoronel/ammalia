4301faf6dca0689a8e122f154a07d8d6d63536ce
Diana Wang <dwang98115@gmail.com>
2016-02-14 00:09:41 -0800

    organized files into events

--- app/assets/js/ammalia.js
+++ app/assets/js/ammalia.js
@@ -13,44 +13,12 @@ Leap.loop(function(frame) {
     scale: 0.25
 });*/
 
-Leap.loop({
-    hand: function(hand) {
-        if (hand.pinchStrength > 0) {
-            var pincher;
-            var closest = 500;
-            var a;
-            var cat;
-            for (var f = 1; f < 5; f++) {
-                current = hand.fingers[f];
-                distance = Leap.vec3.distance(hand.thumb.tipPosition, current.tipPosition);
-                if (current != hand.thumb && distance < closest) {
-                    closest = distance;
-                    pincher = current;
-                    a = hand.pinchStrength;
-                }
-            }
-            if (pincher == hand.indexFinger) {
-                cat = (cats[0] || (cats[0] = new Cat()));
-                console.log(" the finger is index " + pincher.type + "<br />");
-                /*if (0.50 > a) {
-                    //  var cat =( cats[0] || (cats[0] = new Cat()));
-                    cat.setTransform(0, 0, 1.1, 1.1);
-                    //zoom(1.1);
-                    console.log(" the finger is index 22222222222 " + "<br />");
-                }*/
-                if (a >= 0.3 && a <= 0.7) {
-                    // var cat =( cats[0] || (cats[0] = new Cat()));
-                    cat.setTransform(0, 0, 0.9, 0.9);
-                    //zoom(0.9);
-                    console.log(" the finger is index 11111111111 " + "<br />");
-                }
-            }
-        }
-    }
-}).use('screenPosition', {
-    scale: 0.25
-});
 
+// Setup Leap loop with frame callback function
+
+Leap.loop()
+    .use('zoomEvent')
+    .use('swipeEvent');
 /*var controller = new Leap.Controller({
     enableGestures: true
 });
